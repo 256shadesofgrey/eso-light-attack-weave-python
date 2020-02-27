d9ca708e0a84b616975a3900449859fa6a492622
Dmitri Ranfft <ranfft.dmitri@gmail.com>
2020-02-27 02:55:28 +0100

    Add on_release listener to prevent modifier keys from getting stuck

--- eso-light-attack-weave.py
+++ eso-light-attack-weave.py
@@ -21,32 +21,55 @@ block_cancelling_enabled = [0, 0, 0, 0, 0, 0]
 kc = keyboard.Controller()
 mc = mouse.Controller()
 
-ignore_key = False
+ignore_press = False
+ignore_release = False
+
 
 def is_skill_key(key):
-    print("is_skill_key()")
+    print("is_skill_key({0})".format(key))
     for k in skill_keys:
         if "'{0}'".format(k) == "{0}".format(key):
             return True
     return False
 
+
 def on_press(key):
     # Make sure we don't intercept the key we just sent.
-    global ignore_key
-    if ignore_key:
-        ignore_key = False
+    global ignore_press
+    if ignore_press:
+        ignore_press = False
         return
-    ignore_key = True
+    ignore_press = True
     
     if not is_skill_key(key):
         # If it's not a skill key, just pass it through.
-        kc.press(key.char)
+        try:
+            kc.press(key.char)
+        except AttributeError:
+            kc.press(key)
     else:
         # TODO: implement the actual macro here.
         return False
 
+
+def on_release(key):
+    # Make sure we don't intercept the key we just sent.
+    global ignore_release
+    if ignore_release:
+        ignore_release = False
+        return
+    ignore_release = True
+
+    try:
+        kc.release(key.char)
+    except AttributeError:
+        kc.release(key)
+
+
 def main():
-    with keyboard.Listener(on_press=on_press, suppress=True) as listener:
+    with keyboard.Listener(on_press=on_press, on_release=on_release,
+                           suppress=True) as listener:
         listener.join()
+
 
 if __name__ == "__main__":main()
