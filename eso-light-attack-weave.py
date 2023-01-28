5ba3291d18f10eadd627e309da6090061c2bdf2d
Dmitri Ranfft <ranfft.dmitri@gmail.com>
2023-01-28 14:32:30 +0100

    Add minimal implementation of the macro

--- eso-light-attack-weave.py
+++ eso-light-attack-weave.py
@@ -9,6 +9,9 @@ __all__ = []
 # Keys used for the skills.
 skill_keys = ["1", "2", "3", "4", "5", "r"]
 
+# Light attack key.
+la_key = Button.left
+
 # Which skills will be used with weaving (same order as in skill_keys).
 weaving_enabled = [1, 1, 1, 1, 1, 0]
 
@@ -26,9 +29,16 @@ ignore_release = False
 
 
 def is_skill_key(key):
-    print("is_skill_key({0})".format(key))
     for k in skill_keys:
         if "'{0}'".format(k) == "{0}".format(key):
+            return True
+    return False
+
+
+def is_enabled_skill_key(key):
+    for i in range(len(skill_keys)):
+        if "'{0}'".format(skill_keys[i]) == "{0}".format(key) and \
+                weaving_enabled[i] == 1:
             return True
     return False
 
@@ -41,15 +51,22 @@ def on_press(key):
         return
     ignore_press = True
 
-    if not is_skill_key(key):
-        # If it's not a skill key, just pass it through.
+    if not is_enabled_skill_key(key):
+        # If it's not a skill key or the key is disabled, just pass it through.
         try:
             kc.press(key.char)
         except AttributeError:
             kc.press(key)
     else:
-        # TODO: implement the actual macro here.
-        return False
+        # NOTE: Actual macro implementation here.
+        # TODO: Check if la_key requires mouse or keyboard input.
+        mc.click(la_key)
+        try:
+            kc.press(key.char)
+        except AttributeError:
+            kc.press(key)
+
+        # return False
 
 
 def on_release(key):
