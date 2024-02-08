63063e1b6d0ff27169a64a2f27e746e7f6d26b29
Dmitri Ranfft <ranfft.dmitri@gmail.com>
2024-02-08 03:26:38 +0100

    Add skeleton for common key mapping

---
+++ drivers/Mapping_pynput.py
@@ -0,0 +1,16 @@
+from bidict import bidict
+
+from pynput.keyboard import Key
+from pynput.mouse import Button
+
+class Mapping:
+
+    # Takes the library specific identification of the key/button and
+    # returns the common identification string.
+    def getCommon():
+        return
+
+    # Takes the common identification string of the key/button and
+    # returns the library specific identification.
+    def getLibSpecific():
+        return
